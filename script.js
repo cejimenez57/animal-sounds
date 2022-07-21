9836f2eacd0d18bf0eb134372438888cc4472948
Christian <49210329+cejimenez57@users.noreply.github.com>
2022-07-20 23:00:49 -0500

    created function for noises

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// TODO: 
-// 1. Have a unique sound for each animal
-// 2. Download all animal sounds
-// 3. Maybe: Switch Break loop?
-
-// FIXME:
-// 1. Make each sound generate different according to animal
-const animalNoise = document.querySelector('.animal');
-var audio = new Audio("/audio/horse.mp3");
-
-
-animalNoise.addEventListener("click", function(){
+function play(sound){
+    var audio = document.getElementById("audio");
+    audio.setAttribute('src', sound);
     audio.play();
-});
+}
